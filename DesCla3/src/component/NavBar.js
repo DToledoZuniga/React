983460449f6da28e7be1d61fd233f0195cf6b6fd
Diego <Diego@DESKTOP-SI78H6F>
2021-12-21 22:29:26 -0300

    Desafio Clase 3

---
+++ DesCla3/src/component/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './NavBar.css';
+
+function NavBar() {
+    return (
+        <div className='topnav'>
+            <a className='nombreTienda'>Tienda ReactJS</a>
+            <a href="">Inicio</a>
+            <a href="">Productos</a>
+        </div>
+    )
+}
+
+export default NavBar
